42394e14f664058b0f45fa3107e3c191cd24ebc3
Dmitry Kunin <dkunin@avito.ru>
2016-06-22 16:16:02 +0300

    master:Broken app distribution

--- server-start.js
+++ server-start.js
@@ -1,2 +1,14 @@
+'use strict';
+
 const server = require('./server');
-server.start();
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+
+const CONFIG = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname + '/config.yml'), 'utf8'));
+const CollectionStoreClass = require('./modules/store');
+const collectionStore = new CollectionStoreClass(appDir + '/' + CONFIG.server.db);
+const usersStore = new CollectionStoreClass(appDir + '/' + CONFIG.server.usersdb);
+
+server.start(collectionStore, usersStore);
--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const yaml = require('js-yaml');
 const cockieParser = require('cookie-parser');
 const expressSession = require('express-session');
 
-const appDir = path.dirname(require.main.filename);
-const CollectionStoreClass = require('./modules/store');
-
 const {
         getLogin,
         postLogin,
@@ -34,59 +31,55 @@ const CARDBASE = externalapi.cardbase;
 const PRICES = externalapi.prices;
 const app = express();
 
-const collectionStore = new CollectionStoreClass(appDir + '/' + server.db);
-const usersStore = new CollectionStoreClass(appDir + '/' + server.usersdb);
-
-passport.use(new Strategy(
-  function(username, password, cb) {
-      usersStore.findOne({ username }).then(doc => {
-          if (doc.password != password) {
-              return cb(null, false);
-          }
-          return cb(null, doc);
-      }).catch(err => cb(err, false));
-  }));
-
-passport.serializeUser(function(userObj, cb) {
-    cb(null, userObj);
-});
-
-passport.deserializeUser(function(userObj, cb) {
-    var err = null;
-    if (err) {
-        return cb(err);
-    }
-    cb(null, userObj);
-});
-
-app.use(cockieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(expressSession({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}));
-app.use(express.static('./dist'));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.post('/api/login', passport.authenticate('local'), postLogin);
-app.post('/api/newuser', postNewUser(usersStore));
-app.get('/api/login', getLogin);
-app.get('/logout', getLogout);
-app.get('/api/search/:query', getSearch(request, CARDBASE));
-app.get('/api/possible-price', getPossiblePrice(request, PRICES));
-app.get('/api/collection', getCollection(collectionStore));
-app.get('/api/collectionexport', getCollection(collectionStore));
-app.post('/api/addCard', postCardAdd(request, CARDBASE, collectionStore));
-app.post('/api/removeCard', postCardRemove(collectionStore));
-app.post('/api/updateCard', postCardUpdate(collectionStore));
-app.post('/api/importCards', postCardImport(request, CARDBASE, collectionStore));
-
 module.exports = {
     port: () => PORT,
-    start: function() {
+    start: function(collectionStore, usersStore) {
+        passport.use(new Strategy(
+          function(username, password, cb) {
+              usersStore.findOne({ username }).then(doc => {
+                  if (doc.password != password) {
+                      return cb(null, false);
+                  }
+                  return cb(null, doc);
+              }).catch(err => cb(err, false));
+          }));
+
+        passport.serializeUser(function(userObj, cb) {
+            cb(null, userObj);
+        });
+
+        passport.deserializeUser(function(userObj, cb) {
+            var err = null;
+            if (err) {
+                return cb(err);
+            }
+            cb(null, userObj);
+        });
+
+        app.use(cockieParser());
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.use(expressSession({
+            secret: 'keyboard cat',
+            resave: false,
+            saveUninitialized: true
+        }));
+        app.use(express.static('./dist'));
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        app.post('/api/login', passport.authenticate('local'), postLogin);
+        app.post('/api/newuser', postNewUser(usersStore));
+        app.get('/api/login', getLogin);
+        app.get('/logout', getLogout);
+        app.get('/api/search/:query', getSearch(request, CARDBASE));
+        app.get('/api/possible-price', getPossiblePrice(request, PRICES));
+        app.get('/api/collection', getCollection(collectionStore));
+        app.get('/api/collectionexport', getCollection(collectionStore));
+        app.post('/api/addCard', postCardAdd(request, CARDBASE, collectionStore));
+        app.post('/api/removeCard', postCardRemove(collectionStore));
+        app.post('/api/updateCard', postCardUpdate(collectionStore));
+        app.post('/api/importCards', postCardImport(request, CARDBASE, collectionStore));
         app.listen(PORT);
         console.log(`listening on port ${PORT}`);
     }
